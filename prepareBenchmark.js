c824f4b72cd70a7970b60285adf0d9a6a02568d1
Aler Denisov <aler.zampillo@gmail.com>
2018-01-24 10:44:17 +0300

    small fixes to make it works with musereum

--- prepareBenchmark.js
+++ prepareBenchmark.js
@@ -4,106 +4,41 @@ const hdkey = require('ethereumjs-wallet/hdkey')
 const util = require('ethereumjs-util')
 
 
-const numAccounts = process.argv[2] || 1000;
-const numTransactions = process.argv[3] || 10000;
-
-
-const networkId = '0x42';
-const nonce = "0x0000000000000000";
-const difficulty = '0x40000';
-const gasLimit = '0xFFFF0000';
-
-
-const genesisGethTemplate = {
-  "nonce": nonce,
-  "difficulty": difficulty,
-  "mixhash": "0x0000000000000000000000000000000000000000000000000000000000000000",
-  "coinbase": "0x0000000000000000000000000000000000000000",
-  "timestamp": "0x00",
-  "parentHash": "0x0000000000000000000000000000000000000000000000000000000000000000",
-  "extraData": "0x0000000000000000000000000000000000000000000000000000000000000000",
-  "gasLimit": gasLimit,
-  "alloc": {}
-};
-
-
-const genesisParityTemplate = {
-  "name": "test",
-  "engine": {
-    "Ethash": {
-      "params": {
-        "gasLimitBoundDivisor": "0x0400",
-        "minimumDifficulty": "0x4",
-        "difficultyBoundDivisor": "0x0800",
-        "durationLimit": "0x0a",
-        "blockReward": "0x4563918244F40000",
-        "registrar": "",
-        "frontierCompatibilityModeLimit": "0x00"
-      }
-    }
-  },
-  "params": {
-    "maximumExtraDataSize": "0x20",
-    "minGasLimit": "0xFFFF0000",
-    "networkID" : networkId
-  },
-  "genesis": {
-    "seal": {
-      "ethereum": {
-        "nonce": nonce,
-        "mixHash": "0x0000000000000000000000000000000000000000000000000000000000000000"
-      }
-    },
-    "difficulty": difficulty,
-    "author": "0x0000000000000000000000000000000000000000",
-    "timestamp": "0x00",
-    "parentHash": "0x0000000000000000000000000000000000000000000000000000000000000000",
-    "extraData": "0x0000000000000000000000000000000000000000000000000000000000000000",
-    "gasLimit": gasLimit,
-  },
-  "nodes": [ ],
-  "accounts": { }
-}
-
-
+const numAccounts = parseInt(process.argv[2])
+const numTransactions = parseInt(process.argv[3])
+const genesis = require(process.argv[4])
 
 // Generate keys
-const seed = Number(new Date());
-const accounts = [];
+const seed = Number(new Date())
+const accounts = []
 for(let i = 0; i < numAccounts; ++i) {
-  const key = hdkey.fromMasterSeed(seed + '_' + i)._hdkey._privateKey;
+  const key = hdkey.fromMasterSeed(seed + '_' + i)._hdkey._privateKey
   accounts.push({
     key: key,
     addr: util.bufferToHex(util.privateToAddress(key)),
     nonce: 0
-  });
+  })
 }
 
 
 // Fill balances in genesis
-const genesisGeth = Object.assign({}, genesisGethTemplate);
-const genesisParity = Object.assign({}, genesisParityTemplate);
-genesisGeth.alloc = genesisParity.accounts = accounts.reduce(
+genesis.accounts = accounts.reduce(
   (val, acc) => Object.assign(val, {[acc.addr]: {balance: "654321000000000000000"}}),
   {}
-);
+)
 
 
 // write keys and genesis files
-fs.writeFile('genesis.geth.json', JSON.stringify(genesisGeth), err =>
-  err || console.log('genesis.geth.json generated')
-);
-fs.writeFile('genesis.parity.json', JSON.stringify(genesisParity), err =>
-  err || console.log('genesis.parity.json generated')
-);
-
+fs.writeFile(process.argv[4] + '.test', JSON.stringify(genesis), err =>
+  err || console.log(`${process.argv[4]} generated`)
+)
 
 // Generate signed transactions
-const transactions = [];
+const transactions = []
 
 for(let i = 0; i < numTransactions; ++i) {
-  const k = accounts[i % numAccounts];
-  const to = accounts[numAccounts - i % numAccounts - 1].addr;
+  const k = accounts[i % numAccounts]
+  const to = accounts[numAccounts - i % numAccounts - 1].addr
 
   const tx = new Tx({
     from: k.addr,
@@ -113,15 +48,15 @@ for(let i = 0; i < numTransactions; ++i) {
     gasLimit: '0x27100',
     value: '0x12300',
     data: '0x0',
-    chainId: '0x42'
-  });
+    chainId: '0xC0FFEE6'
+  })
 
-  tx.sign(k.key);
-  k.nonce++;
-  transactions.push('0x' + tx.serialize().toString('hex'));
+  tx.sign(k.key)
+  k.nonce++
+  transactions.push('0x' + tx.serialize().toString('hex'))
 }
 
 // Write transactions to file
 fs.writeFile('transactions.txt', transactions.join('\n'), err =>
   err || console.log(numTransactions + ' transactions written to transactions.txt')
-);
+)
--- runBenchmark.js
+++ runBenchmark.js
@@ -1,35 +1,42 @@
 const fs = require('fs')
-const Web3 = require('web3');
+const Web3 = require('web3')
 
-const web3 = new Web3();
-web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
-
-
-const transactionPerSecond = Number.parseInt(process.argv[2] || 10);
-const transactionsPer100Ms = Math.ceil(transactionPerSecond / 10);
-
-console.log((transactionsPer100Ms * 10) + ' transactions per second');
+const web3 = new Web3()
+web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'))
 
 
-const transactions = fs.readFileSync('transactions.txt', 'ascii').split('\n');
-const numTransactions = transactions.length;
+const transactionPerSecond = Number.parseInt(process.argv[2] || 10000)
+const transactionsPer100Ms = Math.ceil(transactionPerSecond / 10)
 
-let stopOnError = false;
-let i = 0;
+console.log((transactionsPer100Ms * 10) + ' transactions per second')
+
+
+const transactions = fs.readFileSync('transactions.txt', 'ascii').split('\n')
+const numTransactions = transactions.length
+
+let stopOnError = false
+let i = 0
 
 function batch() {
-  const maxI = Math.min(i + transactionsPer100Ms, numTransactions);
+  const maxI = Math.min(i + transactionsPer100Ms, numTransactions)
   for (; i < maxI; ++i) {
-
     web3.eth.sendRawTransaction(
       transactions[i],
-      (err,res) => { stopOnError |= !!err }
-    );
+      (err,res) => { 
+        if (err) 
+        {
+          stopOnError = err 
+        }
+      }
+    )
   }
   if (i < numTransactions && !stopOnError) {
-    setTimeout(batch, 100);
+    setTimeout(batch, 100)
+  } else if (stopOnError) {
+    process.stdout.write(JSON.stringify(stopOnError))
   }
-  process.stdout.write('.');
+
+  process.stdout.write('.')
 }
 
-setTimeout(batch, 100);
+setTimeout(batch, 100)
